865ea843b48c8a34a41c4240f8ebde2c704a29e9
Haymanot W <haymanotalemu21@gmail.com>
2020-07-03 07:07:19 -0400

    block of codes to display current weather

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-function buildQueryURL() {
-    var queryURL ="https://api.openweathermap.org/data/2.5/weather?";
-    var queryparams = { "api-key": "a0aca8a89948154a4182dcecc780b513" };
-    queryparams.q = $("#search-city").val().trim();
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryparams));
-    return queryURL + $.param(queryparams);
-
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+// searches the city to see if it exists in the entries from the storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
 }
+//Set up the API key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// Display the curent and future weather to the user after grabing the city form the input.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// Here we create the AJAX call
+function currentWeather(city){
+    // Here we build the URL so we can get a data from server side.
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        console.log(response);
+        // parse the response to display the current weather including the City name. the Date and the weather icon.
+        var weathericon= response.weather[0].icon;
+
+
+    });
+}
+
+
 
 
 
