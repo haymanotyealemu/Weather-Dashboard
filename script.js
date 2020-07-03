7302981e5a238ac1472f8e59a923394af3af9b61
Haymanot W <haymanotalemu21@gmail.com>
2020-07-02 20:22:20 -0400

    building the queryURL

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 
- //Set the API key
-    var APIKey= "7530ba507c7637bcaa306e843e0a6954";
-    // This .on("click") function will trigger the AJAX Call
-    $(".btn").on("click",function(event){
-        // event.preventDefault() can be used to prevent an event's default behavior.
-        // Here, it prevents the search button from trying to submit a form when clicked
-        event.preventDefault();
-        //Grab text the user typed into the search input, 
-        var city= $("#search-city").val().trim();
-        // Here we construct our URL
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-        
-        
-        // Here we run our AJAX call to the OpenWeatherMap API
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response){
-            // Log the queryURL
-            console.log(queryURL);
-            // log the resulting object
-            console.log(response);
+/**
+ * pulls information from the form and build the query URL
+ * @returns {string} URL for NYT API based on form inputs
+ */
+function buildQueryURL() {
+    var queryURL ="https://api.openweathermap.org/data/2.5/weather?";
+    var queryparams = { "api-key": "a0aca8a89948154a4182dcecc780b513" };
+    queryparams.q = $("#search-city").val().trim();
+    console.log("---------------\nURL: " + queryURL + "\n---------------");
+    console.log(queryURL + $.param(queryparams));
+    return queryURL + $.param(queryparams);
 
-        });
+}
 
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
